e76ae0cde426d35b1fdb49c4f90ddf3a1057ad12
Deep Duggal <7544238+deepduggal@users.noreply.github.com>
2024-05-22 16:48:08 -0400

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SessionContext.Provider value={{ session, setSession}}>
+      <SessionContext.Provider value={{ session, setSession }}>
         {/* Not Authorized */}
         {!session.user && <GoogleLogin />}
 
